f0bfc16941aa990c5886411972199f8ff7801763
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2022-01-10 14:41:49 +0100

    started working on index/home page

--- index.php
+++ index.php
@@ -4,30 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Home</title>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid bg-black">
         <div class="row">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Lessen</a>
+            <ul class="col-md-12 nav d-flex justify-content-around align-items-center">
+                <li class="nav-brand">
+                    <img src="images/SRDC-Logo.png" alt="">
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Workshops</a>
+                    <a class="nav-link text-maroon" href="#">Lessen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Parties</a>
+                    <a class="nav-link text-maroon" href="#">Workshops</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Shows</a>
+                    <a class="nav-link text-maroon" href="#">Parties</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Inloggen</a>
+                    <a class="nav-link text-maroon" href="#">Shows</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-maroon" href="#">Inloggen</a>
                 </li>
             </ul>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <img src="images/SRDC-Banner.png" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 btn-group">
+                <a class="btn btn-maroon">Over ons</a>
+                <a class="btn btn-maroon">Contact</a>
+            </div>
         </div>
     </div>
 </body>
